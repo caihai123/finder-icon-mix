04900e897d7ceca6c0272b6b952156d38375a4cb
Cai Hai <lets.move1994@163.com>
2025-05-06 19:44:14 +0800

    【更新】github pages 刷新404，这样好歹首页可以正常刷新

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "/src/pages/Home";
 import About from "/src/pages/About";
 
@@ -7,8 +7,7 @@ const App = () => {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
